dd1a23e886ddc8ebb9721b4f951b43a046976ca6
israeludofia1990 <udofiaisrael170@gmail.com>
2024-08-10 22:40:48 +0100

    created layout for landing page

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-//import './Navbar.css'
+import React, { useState } from 'react'
+import './Navbar.css'
 import logo from '../assets/Assets/Frontend_Assets/fibely1.jpeg'
 import cart_icon from '../assets/Assets/Frontend_Assets/cart_logo.jpeg'
 
 const Navbar = () => {
+    const [menu,setMenu] = useState("shop");
   return (
     <div className='navbar'>
         <div className='nav-logo'>
@@ -11,14 +12,15 @@ const Navbar = () => {
             <p>SHOPPER</p>
         </div>
         <ul className='nav-menu'>
-            <li>Shop</li>
-            <li>Fiction</li>
-            <li>Non Fiction</li>
-            <li>Others</li>
+            <li onClick={()=>{setMenu("shop")}}>Shop {menu==="shop"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("fictions")}}>Fiction {menu==="fictions"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("Non-fictions")}}>Non-Fiction {menu==="Non-fictions"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("others")}}>Others {menu==="others"?<hr/>:<></>}</li>
         </ul>
         <div className='nav-login-cart'>
             <button>Login</button>
             <img src={cart_icon} alt=''/>
+            <div className='nav-cart-count'>0</div>
         </div>
     </div>
   )

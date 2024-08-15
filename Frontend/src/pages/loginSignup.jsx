c87c4709d22a096695fb5f96e617ed813b941b54
israeludofia1990 <udofiaisrael170@gmail.com>
2024-08-15 18:55:55 +0100

    i created login sign up component in loginSignup.jsx

--- Frontend/src/pages/loginSignup.jsx
+++ Frontend/src/pages/loginSignup.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const loginSignup = () => {
   return (
-    <div>
+    <div className='loginsignup'>
+      <div className="divloginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type='text' placeholder='your Name' />
+          <input type='text' placeholder='email address' />
+          <input type='password' placeholder='password' />
+          <input type='password' placeholder='password' />
+        </div>
+        <button>Continue</button>
+        <p className='loginsignup-login'>Already have an account? <span>Login here</span></p>
+        <div className='loginsignup-agree'>
+          <input type="checkbox" name='' id='' />
+          <p>By continuing, i agree to the terms of use and Piracy policy</p>
+        </div>
+      </div>
       
     </div>
   )

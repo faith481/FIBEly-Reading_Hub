509418caae59ac7a4747e56b85aed638df9ee366
israeludofia1990 <udofiaisrael170@gmail.com>
2024-08-16 13:15:14 +0100

    linked book page to items page

--- Frontend/src/context/BookContext.jsx
+++ Frontend/src/context/BookContext.jsx
@@ -14,4 +14,4 @@ const BookContextProvider = (props)=>{
         </BookContext.Provider>
     )
 }
-export default BookContextProvider
+export default BookContextProvider;
--- Frontend/src/pages/Books.jsx
+++ Frontend/src/pages/Books.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { BookContext } from '../context/BookContext'
+import { useParams } from 'react-router-dom';
+import Breadcrum from '../components/Breadcrums/Breadcrum';
 
 const Books = () => {
+  const {all_product} = useContext(BookContext);
+  const {bookId} = useParams();
+  const book = all_product.find((e)=> e.id === Number(bookId))
   return (
     <div>
-      
+      <Breadcrum book={book}/>
     </div>
   )
 }

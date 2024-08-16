c38fa82b17f5dbc07ce3f7550b990efae1d90584
Bruka25 <bkteshome862@gmail.com>
2024-08-16 18:49:59 +0300

    some imrovements on the front end

---
+++ Frontend/src/App.js
@@ -0,0 +1,50 @@
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import LoginSignup from "./pages/loginSignup.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Books from "./pages/Books.jsx";
+import Bookscategory from "./pages/Bookscategory.jsx";
+import Shop from "./pages/shop.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import fiction_banner from "./components/assets/Assets/Frontend_Assets/banner_fiction.png";
+import nonfiction_banner from "./components/assets/Assets/Frontend_Assets/banner_non-fiction.png";
+import others_banner from "./components/assets/Assets/Frontend_Assets/banner_others.png";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route
+            path="/fiction"
+            element={
+              <Bookscategory banner={fiction_banner} category="fiction" />
+            }
+          />
+          <Route
+            path="/Non-fiction"
+            element={
+              <Bookscategory
+                banner={nonfiction_banner}
+                category="non-fiction"
+              />
+            }
+          />
+          <Route
+            path="/others"
+            element={<Bookscategory banner={others_banner} category="others" />}
+          />
+          <Route path="/Books" element={<Books />} />
+          <Route path=":productId" element={<Books />} />
+          <Route path="/cart" element={<cart />} />
+          <Route path="/signup" element={<LoginSignup />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;

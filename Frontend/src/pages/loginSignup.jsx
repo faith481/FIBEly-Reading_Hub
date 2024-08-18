d6f60d9fede306cb5421513eb9c1f8fa1bf3290d
Bruka25 <bkteshome862@gmail.com>
2024-08-19 01:23:18 +0300

    update the front end for elegant landing page

--- Frontend/src/pages/loginSignup.jsx
+++ Frontend/src/pages/loginSignup.jsx
@@ -4,23 +4,23 @@ import { Link } from "react-router-dom";
 
 const Signup = () => {
   return (
-    <div className="loginsignup">
-      <div className="loginsignup-container">
+    <div className="signup">
+      <div className="signup-container">
         <h1>Sign Up</h1>
-        <div className="loginsignup-fields">
+        <div className="signup-fields">
           <input type="text" placeholder="your Name" />
           <input type="text" placeholder="email address" />
           <input type="password" placeholder="password" />
           <input type="password" placeholder="Confirm password" />
         </div>
         <button>Continue</button>
-        <p className="loginsignup-login">
+        <p className="signup-login">
           Already have an account?
           <Link to="/login">
             <button>Login here</button>
           </Link>
         </p>
-        <div className="loginsignup-agree">
+        <div className="signup-agree">
           <input type="checkbox" name="" id="" />
           <p>By continuing, i agree to the terms of use and Piracy policy</p>
         </div>

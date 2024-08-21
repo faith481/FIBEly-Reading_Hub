209b1a3969ea346f5b3ccba73e8d49160e405e49
Bruka25 <bkteshome862@gmail.com>
2024-08-21 18:33:49 +0300

    update the repo for publisher book management frontend

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { useParams } from "react-router-dom";
+//import { useParams } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar.jsx";
 import Signup from "./pages/loginSignup.jsx";
 import Login from "./pages/login.jsx";

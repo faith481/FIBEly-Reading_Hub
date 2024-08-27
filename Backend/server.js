45d3da970e96efceba2e492100fdd7b0df652ea6
Bruka25 <bkteshome862@gmail.com>
2024-08-27 23:47:28 +0300

    some tests for the app

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
-//const RedisStore = require("connect-redis").default;
-//const redisClient = require("./utils/redis");
 const mongoose = require("./utils/db");
-//const authenticateJWT = require("./middleware/middleAuth");
 const route = require("./routes/route");
-//const user = require("./models/userModel");
 const cors = require("cors");
 const upload = require("./middleware/multer");
 const path = require("path");
@@ -17,7 +13,6 @@ app.use(express.json());
 // Session middleware using Redis
 app.use(
   session({
-    //store: new RedisStore({ client: redisClient }),
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
@@ -27,18 +22,6 @@ app.use(
 
 app.use("/", route);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
-//app.use(route);
-
-// Protected Route to be user for JWT authentication
-//app.get("/dashboard", authenticateJWT, (req, res) => {
-//  try {
-//    res.json({ message: `Welcome to the dashboard, ${req.user.userId}` });
-//  } catch (error) {
-//    console.error(error);
-//   res.status(500).json({ error: "An error occurred" });
-// }
-//});
 
 const PORT = process.env.PORT || 5000;
 const IP_ADDRESS = process.env.NODE_IP_ADDRESS || "127.0.0.1";

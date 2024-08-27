ff9c02c6804462e51ffa8702088a27c71b43b62a
leukeng <ekengeffiong21@gmail.com>
2024-08-27 07:10:35 -0700

    remove the backend directory

--- Backend/models/cartModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const cartSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  books: [{ type: mongoose.Schema.Types.ObjectId, ref: "Book" }],
-  createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("Cart", cartSchema);
